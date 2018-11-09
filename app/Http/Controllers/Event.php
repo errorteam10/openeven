1a364e3a54c3d2d03e182b0f109717bfa56b1df2
root <root@openeven.com>
2018-11-09 17:21:35 +0000

    Added Event Controller

--- app/Http/Controllers/Event.php
+++ app/Http/Controllers/Event.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\event as Events;
 
 class Event extends Controller
 {
     //
+
+    public function get_event($request) {
+      if($request != null && $request != '') {
+        $data = json_decode(Events::get()->where('id', $request));
+      }
+      die(json_encode($data));
+    }
 }
