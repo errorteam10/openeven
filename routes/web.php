1a60fb96eeec793bd6ff82a1ac490d6a9e2405fc
root <root@openeven.com>
2018-11-09 17:23:01 +0000

    11-9-2018:10:22AM

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,16 @@ Route::domain('mobile.' . env('app_url') )->group(function () {
     Route::get('mobile', 'Mobile@home');
 });
 
-Route::get('/', function () {
-    return view('pages.home');
-});
 
+// Main App Routes
 Auth::routes();
-
+Route::get('/', function () {return view('pages.home');});
 Route::get('/home', 'User@home')->name('home');
+
+//Event Routes POST/GET/SET
+Route::get('/event/{id}', 'Event@get_event')->name('get_event');
+Route::get('/organizer/{name}', 'Event@get_organizer')->name('get_organizer');
+Route::post('/make/event', 'Event@make_event');
+Route::post('/make/ticket', 'Ticket@make_ticket');
+Route::post('/edit/event', 'Event@edit_event');
+Route::post('/edit/ticket', 'Ticket@edit_ticket');
